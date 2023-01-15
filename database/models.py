08a56cf4d8c115df8d3d341e7ab9a982a2209c18
gorandalex <andrii.gorobets@gmail.com>
2023-01-15 19:22:35 +0200

    models add Grade

--- database/models.py
+++ database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.schema import ForeignKey
@@ -30,6 +30,7 @@ class Student(Base):
     group_id = Column('group_id', ForeignKey(Group.id, ondelete='CASCADE'), nullable=False)
     
     group = relationship(Group, back_populates = 'students')   
+    grades = relationship('Grade', back_populates = 'student')
     
     
 class Subject(Base):
@@ -39,3 +40,15 @@ class Subject(Base):
     teacher_id = Column(Integer, ForeignKey(Teacher.id, ondelete='CASCADE'), nullable=False)
     
     teacher = relationship(Teacher, back_populates = 'subjects')
+    grades = relationship('Grade', back_populates = 'subject')
+
+class Grade(Base):
+    __tablename__ = 'grades'
+    id = Column(Integer, primary_key=True)
+    grade = Column(Integer, nullable=False)
+    create_at = Column(DateTime, nullable=False)
+    student_id = Column(Integer, ForeignKey(Student.id, ondelete='CASCADE'), nullable=False)
+    subject_id = Column(Integer, ForeignKey(Subject.id, ondelete='CASCADE'), nullable=False)
+    
+    student = relationship(Student, back_populates = 'grades')
+    subject = relationship(Subject, back_populates = 'grades')
